5513631dcdf61534c01babc9b38095b3c5261fa7
Kali Pso <kalipso@c3d2.de>
2016-11-08 15:51:16 +0000

    bier hinzugefügt. du kannst nun bier trinken

---
+++ bier.py
@@ -0,0 +1,7 @@
+print("Wie viel bier möchtest du tirnken?")
+a = input("> ")
+i = 0
+while (i<=int(a)):
+    print("Das war dein %s. Bier" % (i,))
+    i += 1
+print("Du hast es geschafft, du hast %s Bier getrunken" % (a,))
